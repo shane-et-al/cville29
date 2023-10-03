5a9bca543033f1047118afefb2377fbdbbb568fe
shane-et-al <ssl2ab@virginia.edu>
2023-10-03 14:53:15 -0400

    strip out strings

--- cville29.py
+++ cville29.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import string
 
 url = "https://charlottesville29.com/five-finds-on-friday/"
 html  = requests.get(url).text
@@ -7,8 +8,8 @@ soup = BeautifulSoup(html, features="html.parser")
 
 entries = soup.find_all('p', attrs={'style':'padding-left: 30px;'})
 
-restaurants = {}
-restaurantname = {}
+restaurantcount = {}
+restauranturl = {}
 restaurantrecs = {}
 
 for entry in entries:
@@ -16,28 +17,30 @@ for entry in entries:
         continue
     restaurant = entry.find('a')
     if restaurant:
+        rname = restaurant.text.strip().strip(string.punctuation)
+        rurl = restaurant["href"]
         item = entry.find('strong')
         if not item:
             item = entry.find('b')
-        if restaurant['href'] in restaurants:
-            restaurants[restaurant['href']] += 1
+        if rname in restaurantcount:
+            restaurantcount[rname] += 1
             if item:
-                restaurantrecs[restaurant['href']].append(item.text.strip())
+                restaurantrecs[rname].append(item.text.strip().strip(string.punctuation))
         else:
-            restaurants[restaurant['href']] = 1
-            restaurantname[restaurant['href']] = restaurant.text
+            restaurantcount[rname] = 1
+            restauranturl[rname] = rurl
             if item:
-                restaurantrecs[restaurant['href']] = [(item.text.strip())]
+                restaurantrecs[rname] = [(item.text.strip().strip(string.punctuation))]
 
-restaurants = sorted(restaurants.items(), key=lambda x:x[1], reverse=True)
+restaurantcount = sorted(restaurantcount.items(), key=lambda x:x[1], reverse=True)
 
 finds = []
-for r in restaurants:
+for r in restaurantcount:
     print(r)
     if r[0] in restaurantrecs:
-        finds.append({"name":restaurantname[r[0]],"url":r[0],"times picked":r[1],"recommendations":restaurantrecs[r[0]]})
+        finds.append({"name":r[0],"url":restauranturl[r[0]],"times picked":r[1],"recommendations":restaurantrecs[r[0]]})
     else:
-        finds.append({"name":restaurantname[r[0]],"url":r[0],"times picked":r[1],"recommendations":[]})
+        finds.append({"name":r[0],"url":restauranturl[r[0]],"times picked":r[1],"recommendations":[]})
 
 import json
 json.dump(finds,open("./finds.json","w"))
