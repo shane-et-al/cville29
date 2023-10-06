4d27971ff141ba020bfc6fa23bd8d3bb19d74d6e
shane-et-al <ssl2ab@virginia.edu>
2023-10-05 22:05:35 -0400

    add find date and finder

--- cville29.py
+++ cville29.py
@@ -7,6 +7,18 @@ import re
 def cleanup(s):
     return s.replace("’","'").strip().strip(string.punctuation)
 
+def find_article(element):
+    # Try to find the link to the original article so we can extract the date
+    previous_ps=element.find_all_previous('p')
+    for i in range(7):
+        if len(previous_ps)<=i:
+            break
+        anchor = previous_ps[i].find("a",href=re.compile("charlottesville29.com\/[0-9]{4}\/[0-9]{2}\/[0-9]{2}\/"))
+        if anchor and len(previous_ps[i].findAll(string=True, recursive=False))==0:
+            return anchor
+    # If we can't find it, we can't find it. There are some egregious HTML errors in here.
+    return None
+
 url = "https://charlottesville29.com/five-finds-on-friday/"
 html  = requests.get(url).text
 soup = BeautifulSoup(html, features="html.parser")
@@ -23,30 +35,46 @@ rnames = set()
 with open("./metadata.json","r") as infile:
     METADATA = json.load(infile)
 
-for entry in entries:
-    if not str.isnumeric(entry.text[0]):
+for element in entries:
+    if not str.isnumeric(element.text[0]):
         continue
+    
+    article = find_article(element)
+    if article:
+        date = "/".join(article["href"].split("/")[3:6])
+        finder = article.text
+    else:
+        date = "unknown"
+        finder = "unknown"
+    
+    
     # Limitations: Sometimes people recommend more than one restaurant; we'll only count the first one because it's harder to distinguish subsequent recommendations and regular links
     # Sometimes, there's no link (because there's no website, because of neglect, or because it's "wife's meatballs"), which we won't count at all.
-    restaurant = entry.find('a')
+    item = element.find('strong')
+    if not item:
+        item = element.find('b')
+    if not item:
+        continue
+    restaurant = item.find_next_sibling('a')
     if restaurant:
         rname = cleanup(restaurant.text)
         if rname in METADATA["name_normalization"]:
             rname = METADATA["name_normalization"][rname]
         rnames.add(rname)
         rurl = restaurant["href"]
-        item = entry.find('strong')
-        if not item:
-            item = entry.find('b')
+        
+        rec_text = restaurant.next_sibling.text.strip().lstrip(".").strip().strip("“”\"")
+
+        recommendation = {"item":cleanup(item.text),"date":date,"finder":finder,"recommendation":rec_text}
         if rname in restaurantcount:
             restaurantcount[rname] += 1
             if item:
-                restaurantrecs[rname].append(cleanup(item.text))
+                restaurantrecs[rname].append(recommendation)
         else:
             restaurantcount[rname] = 1
             restauranturl[rname] = rurl
             if item:
-                restaurantrecs[rname] = [cleanup(item.text)]
+                restaurantrecs[rname] = [recommendation]
 
 restaurantcount = sorted(restaurantcount.items(), key=lambda x:x[1], reverse=True)
 
